3882a1ac098a5d5e15fbe5b841e4385ef63227af
Henrique Cardamone <54248871+hcardamone@users.noreply.github.com>
2022-07-09 12:47:51 -0300

    adjusting game title id

--- resources/get-data/getvalue_variables.py
+++ resources/get-data/getvalue_variables.py
@@ -2,5 +2,4 @@
 XBOX_HOME                   =   'https://www.xbox.com/en-us/games/all-games'
 XBOX_TOPMENU                =   "//button[contains(@id,'uhfCatLogoButton')]"
 XBOX_GC_TITLE               =   'Xbox Games Catalog: All Games | Xbox'
-XBOX_GAME_TITLE             =   "//a[@data-bi-id='n1c22c5c2m1r5a3']"
-XBOX_GAME_VALUE             =   "//a[contains(.,'Grand Theft Auto V: Premium Edition$29.99')]"
+XBOX_GAME_TITLE             =   "//a[@data-bi-name='grand theft auto v: premium edition']"
