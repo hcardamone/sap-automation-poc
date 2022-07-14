4aefb463f88b90ae7163dd43a7ffe115e67c174e
Henrique Cardamone <54248871+hcardamone@users.noreply.github.com>
2022-07-14 00:39:02 -0300

    adding validate the "game" information

--- resources/get-data/getvalue_variables.py
+++ resources/get-data/getvalue_variables.py
@@ -1,14 +1,15 @@
 # Get Value - Home Page Xbox Catalog var
-FIREFOX_PATH                =   "C:\\Users\\henri\\Documents\\SAP\\sap-automation-poc\\env\\geckodriver.exe"
 XBOX_HOME                   =   'https://www.xbox.com/en-us/games/all-games'
 XBOX_TOPMENU                =   "//h1[@class='c-heading-1a green-c'][contains(.,'XBOX GAMES CATALOG')]"
 XBOX_GC_TITLE               =   'Xbox Games Catalog: All Games | Xbox'
 XBOX_GENRE_FTR              =   "xpath=//div[@id='filter-genre']/div/button"
 XBOX_ACT_ADV                =   "xpath=//ul[@id='genreSelect']/li/a/span"
-IS_XBOX_ACT_ADV             =   "//img[@alt='box shot of Grand Theft Auto V: Premium Edition']"
 XBOX_SEARCH_BAR             =   "xpath=//input[@name='search-field']"
 XBOX_GAME_NAME              =   'Grand Theft Auto V'
 XBOX_SEARCH_BTN             =   "xpath=//button[@name='search-button']"
-XBOX_GAME_ID                =   "xpath=//a[contains(@href, 'https://www.xbox.com/en-us/games/store/grand-theft-auto-v/9NXMBTB02ZSF')]"
+XBOX_GAME_ID                =   "//img[@src='//store-images.s-microsoft.com/image/apps.8135.66515090704019777.7fa547c1-c211-4229-a4d3-3ceef762e0a4.0bb0ac0a-9d63-4d91-8e53-f2e39a040bcd?w=200']"
 XBOX_COOKIES                =   "/html/body/div[1]/div/div/div[2]/div/div/div/div/div/div/div/div[2]/button[1]"
+XBOX_GAME_ELEMENT           =   "//a[@data-bi-name='grand theft auto v: premium edition']"
+IS_XBOX_GEN_DISPLAYED       =   "Action & adventure"
+IS_XBOX_GAME_DISPLAYED      =   '//*[@id="hero-10"]/div/div/h1'
 EXPLICIT_WAIT               =   '30 s'
