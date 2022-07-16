a3b26bb5daa5e28c42fc4c85fae828d248f8ee9e
Henrique Cardamone <54248871+hcardamone@users.noreply.github.com>
2022-07-15 23:53:39 -0300

    adjusting code

--- resources/customlib/StringExtended/StringExtended.py
+++ resources/customlib/StringExtended/StringExtended.py
@@ -13,7 +13,9 @@ import os
 def split_string_custom(string_input, flag=0):
     chunks, chunk_size = len(string_input), len(string_input)//flag
     # noinspection PyUnusedLocal
-    splitcustom = [string_input[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
+    splitcustom = []
+    for i in range(0, chunks, chunk_size):
+        splitcustom.append(string_input[i:i + chunk_size])
     print(splitcustom)
     return  splitcustom
 
