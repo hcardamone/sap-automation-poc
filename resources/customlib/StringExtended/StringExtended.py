1ead0ef7c37db59ba946d4ce0cfaf93078da174f
Henrique Cardamone <54248871+hcardamone@users.noreply.github.com>
2022-07-15 23:58:19 -0300

    adjusting code

--- resources/customlib/StringExtended/StringExtended.py
+++ resources/customlib/StringExtended/StringExtended.py
@@ -1,14 +1,4 @@
-from ast import keyword
 from robot.libraries.String import *
-import pythoncom
-from pythoncom import com_error
-from robot.api import logger
-import win32com.client
-import time
-import robot.libraries.Screenshot as screenshot
-import os
-
-#class StringLibraryExtended (String):
 
 def split_string_custom(string_input, flag=0):
     chunks, chunk_size = len(string_input), len(string_input)//flag
