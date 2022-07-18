3350ba8df7280f16c00ae98bbb8abff3f48c0b9b
Henrique Cardamone <henrique.cardamonetec@gmail.com>
2022-07-18 01:21:31 -0300

    adjusting code

--- resources/customlib/StringExtended/StringExtended.py
+++ resources/customlib/StringExtended/StringExtended.py
@@ -1,11 +1,24 @@
 from robot.libraries.String import *
 
-def split_string_custom(string_input, flag=0):
+def split_list_custom(string_input, flag=0):
     chunks, chunk_size = len(string_input), len(string_input)//flag
     # noinspection PyUnusedLocal
     splitcustom = []
     for i in range(0, chunks, chunk_size):
         splitcustom.append(string_input[i:i + chunk_size])
-    print(splitcustom)
+    print(splitcustom[i])
     return  splitcustom
+
+def get_line_count_custom(string):
+    list = string
+    result = [c for c in list[0]]
+    return list
+
+def remove_list_using_regexp(self, string_input, *patterns):
+    list = string_input
+    result = [c for c in list[0]]
+    # noinspection PyUnusedLocal
+    for pattern in patterns:
+        string_input = self.remove_list_using_regexp(list, pattern, '')
+    return result, string_input
 
