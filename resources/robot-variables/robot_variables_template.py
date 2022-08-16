c9cdbf11f7bd45655cd4acb45cf3bd0bc5405390
Henrique Cardamone <henrique.cardamonetec@gmail.com>
2022-08-16 02:59:44 -0300

    adjustments finished, 2 cases tests PASS

--- resources/robot-variables/robot_variables_template.py
+++ resources/robot-variables/robot_variables_template.py
@@ -11,7 +11,3 @@ MULTIPLE_LOGON_ACTION = "endOthersOption_rdi"    # Options:
 # #                                                continueOption_rdi:  Continue with this logon, without ending any other logons in system
 # #                                                terminateOption_rdi: Terminate this logon. This will cause the test run to fail.
 EXPLICIT_WAIT = "0 seconds"
-MOCKDIR           =     '../mock/'
-ECTDIR            =     '../extract-data/'
-TRFDIR            =     '../transform-data/'
-EXPECTED_RESPONSE =     '../mock/data-gamectg-mock.json'
